1a9a8e403cdf05cb4819c705d9650578a968d89e
Kevin_Dietmeyer <kevindietmeyer@gmail.com>
2023-07-22 15:29:28 -0400

    Updated try catch after deploying API

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,31 +1,46 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Exercises() {
-  const [exerciseList, setExerciseList] = useState()
-  const [message, setMessage] = useState('This is a test message from exercises component')
+  const [exerciseList, setExerciseList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getExercises = async (workout) => {
-    setMessage('Creating workout...')
-    setExerciseList()
-    const response = await fetch(`https://hang-bar-db.web.app/exercise/${workout}`)
-    const data = await response.json()
-    setExerciseList(data)
-  }
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://hang-bar-db.web.app/api/exercises`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch data from the server.");
+      }
+      const data = await response.json();
+      setExerciseList(data);
+    } catch (err) {
+      setError("An error occurred while fetching data.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <main>
-  
-  <h1 className='border bg-red-600'>This is an H1 test in app.js</h1>
-  
-  <div className="button-container">
-      <button onClick={() => getExercises('generate workout')}>Generate Workout</button>
-  </div>
-    
-    
-    
-    
-    
+      <h1 className="border bg-red-600">This is an H1 test in app.js</h1>
+      <div className="button-container">
+        <button onClick={() => getExercises("generate-workout")}>
+          Generate Workout
+        </button>
+      </div>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {exerciseList.length > 0 && (
+        <ul>
+          {exerciseList.map((exercise) => (
+            <li key={exercise.id}>
+              Name: {exercise.nameOfExercise}, Type: {exercise.exerciseType}
+            </li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
-
---
+++ src/components/Results.jsx
@@ -0,0 +1,7 @@
+export default function Results() {
+  return(
+    <>
+    <h2>This is a test of the results page</h2>
+    </>
+  )
+}
