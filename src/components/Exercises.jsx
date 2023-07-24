57a66f87af48c4321180df3cf37c14ca3aebb7f7
Kevin_Dietmeyer <kevindietmeyer@gmail.com>
2023-07-24 10:11:21 -0400

    Updated styling tabs center

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -11,12 +11,12 @@ export default function Exercises() {
     try {
       const response = await fetch(`https://hang-bar-db.web.app/api/exercises`);
       if (!response.ok) {
-        throw new Error("Failed to fetch.");
+        throw new Error("Didn't fetch.");
       }
       const data = await response.json();
       setExerciseList(data);
     } catch (err) {
-      setError("An error occurred.");
+      setError("An error just happened.");
     } finally {
       setLoading(false);
     }
@@ -27,10 +27,11 @@ export default function Exercises() {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="text-center mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
-            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+            <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Hang From the Bar</h1>
+            <button className="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Generate Workout</button>
           </div>
-          <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          <div className="container flex flex-col items-center justify-center w-full mx-auto">
+
             <div className="p-2 sm:w-1/2 w-full">
               <div className="bg-gray-100 rounded flex p-4 h-full items-center">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
@@ -73,20 +74,11 @@ export default function Exercises() {
                   <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                   <path d="M22 4L12 14.01l-3-3"></path>
                 </svg>
-                <span className="title-font font-medium">Pack Truffaut Blue</span>
-              </div>
-            </div>
-            <div className="p-2 sm:w-1/2 w-full">
-              <div className="bg-gray-100 rounded flex p-4 h-full items-center">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                  <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                  <path d="M22 4L12 14.01l-3-3"></path>
-                </svg>
                 <span className="title-font font-medium">The Catcher In The Rye</span>
               </div>
             </div>
           </div>
-          <button className="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+          <button className="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">Submit</button>
         </div>
       </section>
     </main>
